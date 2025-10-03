b276cca3ab2a97641b1cdd56917f48276f167aea
s1dm4n <roma.zavvy2016@gmail.com>
2025-10-03 18:16:00 +0300

    Added format date for dateLong

--- .eleventy.js
+++ .eleventy.js
@@ -22,11 +22,33 @@ module.exports = function(eleventyConfig) {
     return DateTime.fromJSDate(new Date(dateObj)).toFormat(format);
   });
 
+  const { DateTime } = require("luxon");
+
   eleventyConfig.addFilter("dateLong", (dateObj) => {
     if (!dateObj) return "Дата не указана";
-    return DateTime.fromJSDate(new Date(dateObj))
-      .setLocale("ru")
-      .toFormat("d LLLL yyyy");
+
+    // Массив месяцев в родительном падеже
+    const monthsGenitive = [
+      "января",
+      "февраля",
+      "марта",
+      "апреля",
+      "мая",
+      "июня",
+      "июля",
+      "августа",
+      "сентября",
+      "октября",
+      "ноября",
+      "декабря",
+    ];
+
+    const dt = DateTime.fromJSDate(new Date(dateObj)).setLocale("ru");
+    const day = dt.toFormat("d"); // День
+    const month = monthsGenitive[dt.month - 1]; // Месяц в родительном падеже (dt.month начинается с 1)
+    const year = dt.toFormat("yyyy"); // Год
+
+    return `${day} ${month} ${year}`;
   });
 
   eleventyConfig.addFilter("wrapParagraphs", (content) => {
